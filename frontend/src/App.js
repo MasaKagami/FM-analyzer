bc110c721a0a907994030232e81a0eac6c452be9
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-09 15:16:37 -0400

    yes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/navbar/NavBar';
 function App() {
   return (
     <div>
-      <HelloWorld />
+      <NavBar />
     </div>
   );
 }
--- frontend/src/components/navbar/NavBar.js
+++ frontend/src/components/navbar/NavBar.js
@@ -1,1 +1,13 @@
 import './NavBar.css';
+
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <section id='navbar'>
+        <span>FM-ANALYZER</span>
+    </section>
+  )
+}
+
+export default NavBar
