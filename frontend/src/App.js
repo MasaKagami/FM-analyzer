73a2559e5f518bc9cbefddb633dffcbf1112ad90
MasaKagami <97225192+MasaKagami@users.noreply.github.com>
2024-06-09 15:10:42 -0400

    first update

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+import NavBar from './components/navbar/NavBar';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
